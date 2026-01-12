ce1365115408a5f82088d189e1d4c7bbaf3b6ef5
makp <vemmaks84@gmail.com>
2026-01-12 13:52:22 +0100

    new comit

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: 'node_modules/next/dist/bin/next',
     args: 'start -p 3000 -H 0.0.0.0',
     interpreter: 'node',
-    cwd: 'C:/Users/vem/pms-stable/frontend',
+    // cwd defaults to the directory where this ecosystem.config.js file is located
     env: {
       NODE_ENV: 'production',
       NEXT_PUBLIC_API_URL: 'http://160.226.0.67/:8000/api',
